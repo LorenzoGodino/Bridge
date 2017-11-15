513602a5a870bfb11b1cf597002c4168651131d5
LorenzoGodino <LorenzoGodino@gmail..com>
2017-11-15 18:21:02 +0100

    Subida Version 2

---
+++ bridgeWeb/frontend/src/app/auth/services/auth.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+import {Observable} from 'rxjs/Observable';
+
+import {User} from '../../core/model/user';
+import {API_URL} from '../../app.component';
+
+@Injectable()
+export class AuthService {
+
+    isLoggedIn = false;
+
+    constructor(private http: Http) {
+    }
+
+    private static handleError(error: any) {
+        const errorMessage = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : `Server error`;
+        console.log(errorMessage);
+        return Observable.throw(errorMessage);
+    }
+
+    login(user: User): Observable<boolean> {
+        return this.http.post(API_URL + '/login', user)
+            .map(response => response.json())
+            .map((currentUser: User) => {
+                if (!user.isNull(currentUser)) {
+                    this.isLoggedIn = true;
+                    return true;
+                } else {
+                    this.isLoggedIn = false;
+                    return false;
+                }
+            })
+            .catch(AuthService.handleError);
+    }
+
+    logOut(): Observable<boolean> {
+        this.isLoggedIn = !this.isLoggedIn;
+        return Observable.of(false);
+    }
+
+    register(user: User): Observable<boolean> {
+        return this.http.post(API_URL + '/register', user)
+            .map(response => response.json() as User)
+            .map(currentUser => !user.isNull(currentUser))
+            .catch(AuthService.handleError);
+    }
+}
