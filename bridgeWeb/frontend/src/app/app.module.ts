b512e4de78a30de4a97ca14498e86dfd78523b1d
LorenzoGodino <LorenzoGodino@gmail..com>
2017-11-17 14:44:18 +0100

    Subida del Día de Hoy

--- bridgeWeb/frontend/src/app/app.module.ts
+++ bridgeWeb/frontend/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from './core/core.module';
 import {AuthModule} from './auth/auth.module';
 import {HomeModule} from './core/components/home/home.module';
 import {routing} from './app.routing';
 import {AppComponent} from './app.component';
-import {AccordionModule , PanelModule,ButtonModule,RadioButtonModule,PasswordModule} from 'primeng/primeng';
+import {InputTextModule, AccordionModule , PanelModule, ButtonModule, RadioButtonModule, PasswordModule} from 'primeng/primeng';
 import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    InputTextModule,
     AccordionModule,
     PanelModule,
     ButtonModule,
     RadioButtonModule,
-	 	PasswordModule,
+    PasswordModule,
     CoreModule,
     AuthModule,
     HomeModule,
