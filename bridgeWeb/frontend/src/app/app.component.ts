d92bf3050d7c494ff1deab464f21522f366cdfd8
LorenzoGodino <LorenzoGodino@gmail..com>
2017-11-23 18:06:58 +0100

    Errores de Compilacion

--- bridgeWeb/frontend/src/app/app.component.ts
+++ bridgeWeb/frontend/src/app/app.component.ts
@@ -3,9 +3,10 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component.html',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
+        
 }
 
 export const API_URL = 'http://localhost:8080/api';
--- bridgeWeb/frontend/src/app/auth/components/login-form/login-form.component.ts
+++ bridgeWeb/frontend/src/app/auth/components/login-form/login-form.component.ts
@@ -6,8 +6,7 @@ import {User} from '../../../core/model/user';
 
 @Component({
     selector: 'app-login-form',
-    templateUrl: 'login-form.component.html',
-    styleUrls: ['login-form.component.css']
+    templateUrl: 'login-form.component.html'
 })
 export class LoginFormComponent implements OnInit {
 
