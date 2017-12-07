bec3a319282d4c1b73e5c68f51fdfeeadf2ba092
LorenzoGodino <LorenzoGodino@gmail.com>
2017-12-07 14:46:00 +0100

    Reajuste Finales

--- bridgeFront/src/app/app.module.ts
+++ bridgeFront/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './component/main/app.component';
+import { LoginComponent } from './component/login/login.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AppRoutingModule,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
