eaa8fa7f7588af8978c6b802b6385dcf51d681d6
LorenzoGodino <LorenzoGodino@gmail.com>
2017-12-07 10:34:50 +0100

    Delete Restructuracion

--- bridgeWeb/frontend/src/app/auth/components/auth/auth.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    templateUrl: 'auth.component.html',
-    styleUrls: ['auth.component.css']
-})
-export class AuthComponent { }
