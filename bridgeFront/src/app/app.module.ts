131f976f5524e0f834b48d7d3ee52d14c86c84ea
LorenzoGodino <LorenzoGodino@gmail.com>
2018-01-05 22:39:56 +0100

    Definitive Intellij

--- bridgeFront/src/app/app.module.ts
+++ bridgeFront/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-
+import {InputTextModule} from 'primeng/primeng';
 import { AppComponent } from './component/main/app.component';
 import { LoginComponent } from './component/login/login.component';
 import {PasswordModule} from 'primeng/primeng';
@@ -14,7 +14,7 @@ import {PasswordModule} from 'primeng/primeng';
 
     ],
     imports: [
-        BrowserModule,BrowserAnimationsModule, AppRoutingModule,PasswordModule
+        BrowserModule,BrowserAnimationsModule, InputTextModule,AppRoutingModule,PasswordModule
     ],
     providers: [],
     bootstrap: [AppComponent]
