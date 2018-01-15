8df440b81d8927b4453ad4d33ed8330a6d01395d
LorenzoGodino <LorenzoGodino@gmail..com>
2018-01-15 19:12:28 +0100

    Existe un Error en el Angular Cli que genera una version que le da problema con schematic lo han corregido en la 0.0.48 pero Angular Cli requiere una versión con Errores

--- bridgeFront/src/app/services/impl/auth.service.ts
+++ bridgeFront/src/app/services/impl/auth.service.ts
@@ -1,3 +1,4 @@
+///<reference path="../interfaces/IAuth.services.ts"/>
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
--- bridgeFront/src/app/services/interfaces/IAuth.services.ts
+++ bridgeFront/src/app/services/interfaces/IAuth.services.ts
@@ -2,6 +2,5 @@ import {Observable} from 'rxjs/Observable';
 import {User} from '../../model/user';
 
 export interface IAuthService {
-  static handleError(error: any);
   login(user): Observable<User>;
 }
