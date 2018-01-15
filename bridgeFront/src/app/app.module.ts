e2dddc3f33b37ece98ffffd6c96618a1a217a522
LorenzoGodino <LorenzoGodino@gmail..com>
2018-01-15 13:43:33 +0100

    Acabado la enmaquetación falta sustituir el Pie.

--- bridgeFront/src/app/app.module.ts
+++ bridgeFront/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import {InputTextModule} from 'primeng/primeng';
-import { AppComponent } from './component/main/app.component';
-import { LoginComponent } from './component/login/login.component';
-import {PasswordModule} from 'primeng/primeng';
-import {ButtonModule} from 'primeng/primeng';
-@NgModule({
-    declarations: [
-        AppComponent,
-        LoginComponent
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {ButtonModule, InputTextModule, PasswordModule} from 'primeng/primeng';
+import {AppComponent} from './component/main/app.component';
+import {LoginComponent} from './component/login/login.component';
 
-    ],
-    imports: [
-        BrowserModule,BrowserAnimationsModule, InputTextModule,AppRoutingModule,PasswordModule,ButtonModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, InputTextModule, AppRoutingModule, PasswordModule, ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
